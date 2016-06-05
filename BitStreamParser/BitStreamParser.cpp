09e4f0d9a2441e574d9308eb9f9ecd85871bc469
nhoughto5 <nhoughto5@gmail.com>
2016-06-05 14:06:41 -0700

    First Change

--- BitStreamParser/BitStreamParser.cpp
+++ BitStreamParser/BitStreamParser.cpp
@@ -2,10 +2,18 @@
 //
 
 #include "stdafx.h"
-
+#include <stdio.h>      /* printf */
+#include <stdlib.h>     /* system, NULL, EXIT_FAILURE */
 
 int main()
 {
-    return 0;
+	int i;
+	printf("Checking if processor is available...");
+	if (system(NULL)) puts("Ok");
+	else exit(EXIT_FAILURE);
+	printf("Executing command DIR...\n");
+	i = system("dir");
+	printf("The value returned was: %d.\n", i);
+	return 0;
 }
 

57dd5c7414ecc6246072cda6431c1b532d77ffd3
nhoughto5 <nhoughto5@gmail.com>
2016-06-21 18:15:18 -0700

    XST working

--- BitStreamParser/BitStreamParser.cpp
+++ BitStreamParser/BitStreamParser.cpp
@@ -11,8 +11,8 @@
 #include <AccCtrl.h>
 #include <Aclapi.h>
 
-//Command Line : sch2hdl -batch C:/Users/Nick/Documents/XilinxProjects/SingleItemTest/sch2HdlBatchFile
-//Command Line : xst -ifn C:/Users/Nick/Documents/XilinxProjects/SingleItemTest/item.xst -ofn C:/Users/Nick/Documents/XilinxProjects/SingleItemTest/item.syr
+//Command Line : sch2hdl -batch C:/Users/Nick/Desktop/NickTop/HomeWork/MASc/FPGAAutomationImplementation/BitStreamParser/BitStreamParser/Xilinx/SingleItemTest/sch2HdlBatchFile
+//Command Line : xst -ifn C:/Users/Nick/Desktop/NickTop/HomeWork/MASc/FPGAAutomationImplementation/BitStreamParser/BitStreamParser/Xilinx/SingleItemTest/item.xst -ofn C:/Users/Nick/Desktop/NickTop/HomeWork/MASc/FPGAAutomationImplementation/BitStreamParser/BitStreamParser/Xilinx/SingleItemTest/item.syr
 //Command Line : ngdbuild - dd _ngo - nt timestamp - uc item.ucf - p xa3s100e - vqg100 - 4 "item.ngc" item.ngd
 //Command Line : C : \Xilinx\14.7\ISE_DS\ISE\bin\nt64\unwrapped\ngdbuild.exe - dd _ngo - nt timestamp - uc item.ucf - p xa3s100e - vqg100 - 4 item.ngc item.ngd
 //Command Line : map - p xa3s100e - vqg100 - 4 - cm area - ir off - pr off - c 100 - o item_map.ncd item.ngd item.pcf
@@ -20,71 +20,108 @@
 //Command Line : trce - v 3 - s 4 - n 3 - fastpaths - xml item.twx item.ncd - o item.twr item.pcf - ucf item.ucf
 //Command Line : bitgen - f item.ut item.ncd
 
+//std::string path = "C:/Users/Nick/Desktop/NickTop/HomeWork/MASc/FPGAAutomationImplementation/BitStreamParser/BitStreamParser/Xilinx/SingleItemTest/";
+std::string path = "C:\\Users\\Nick\\Desktop\\NickTop\\HomeWork\\MASc\\FPGAAutomationImplementation\\BitStreamParser\\BitStreamParser\\Xilinx\\SingleItemTest\\";
+std::wstring wPath = L"C:\\Users\\Nick\\Desktop\\NickTop\\HomeWork\\MASc\\FPGAAutomationImplementation\\BitStreamParser\\BitStreamParser\\Xilinx\\SingleItemTest\\";;
+//std::string path = "Xilinx/SingleItemTest/";
 void convertSchematicToHDL() {
 	printf("Executing sch2hdl\n");
-	system("sch2hdl -batch C:/Users/Nick/Documents/XilinxProjects/SingleItemTest/sch2HdlBatchFile");
+	system(("sch2hdl -batch " + path + "sch2HdlBatchFile").c_str());
 }
-void CreateDirectoryWithUserFullControlACL(std::string path)
+
+size_t ExecuteProcess(std::wstring FullPathToExe, std::wstring Parameters, size_t SecondsToWait)
 {
-	SID_IDENTIFIER_AUTHORITY SIDAuthWorld = SECURITY_WORLD_SID_AUTHORITY;
-	PSID everyone_sid = NULL;
-	AllocateAndInitializeSid(&SIDAuthWorld, 1, SECURITY_WORLD_RID,
-		0, 0, 0, 0, 0, 0, 0, &everyone_sid);
+	size_t iMyCounter = 0, iReturnVal = 0, iPos = 0;
+	DWORD dwExitCode = 0;
+	std::wstring sTempStr = L"";
 
-	EXPLICIT_ACCESS ea;
-	ZeroMemory(&ea, sizeof(EXPLICIT_ACCESS));
-	ea.grfAccessPermissions = SPECIFIC_RIGHTS_ALL | STANDARD_RIGHTS_ALL;
-	ea.grfAccessMode = SET_ACCESS;
-	ea.grfInheritance = NO_INHERITANCE;
-	ea.Trustee.TrusteeForm = TRUSTEE_IS_SID;
-	ea.Trustee.TrusteeType = TRUSTEE_IS_WELL_KNOWN_GROUP;
-	ea.Trustee.ptstrName = (LPWSTR)everyone_sid;
+	/* - NOTE - You should check here to see if the exe even exists */
 
-	PACL acl = NULL;
-	SetEntriesInAcl(1, &ea, NULL, &acl);
+	/* Add a space to the beginning of the Parameters */
+	if (Parameters.size() != 0)
+	{
+		if (Parameters[0] != L' ')
+		{
+			Parameters.insert(0, L" ");
+		}
+	}
 
-	PSECURITY_DESCRIPTOR sd = (PSECURITY_DESCRIPTOR)LocalAlloc(LPTR,
-		SECURITY_DESCRIPTOR_MIN_LENGTH);
-	InitializeSecurityDescriptor(sd, SECURITY_DESCRIPTOR_REVISION);
-	SetSecurityDescriptorDacl(sd, TRUE, acl, FALSE);
+	/* The first parameter needs to be the exe itself */
+	sTempStr = FullPathToExe;
+	iPos = sTempStr.find_last_of(L"\\");
+	sTempStr.erase(0, iPos + 1);
 
-	SECURITY_ATTRIBUTES sa;
-	sa.nLength = sizeof(SECURITY_ATTRIBUTES);
-	sa.lpSecurityDescriptor = sd;
-	sa.bInheritHandle = FALSE;
+	Parameters = sTempStr.append(Parameters);
 
-	CreateDirectoryA(path.c_str(), &sa);
+	/* CreateProcessW can modify Parameters thus we allocate needed memory */
+	wchar_t * pwszParam = new wchar_t[Parameters.size() + 1];
+	if (pwszParam == 0)
+	{
+		return 1;
+	}
+	const wchar_t* pchrTemp = Parameters.c_str();
+	wcscpy_s(pwszParam, Parameters.size() + 1, pchrTemp);
 
-	FreeSid(everyone_sid);
-	LocalFree(sd);
-	LocalFree(acl);
+	/* CreateProcess API initialization */
+	STARTUPINFOW siStartupInfo;
+	PROCESS_INFORMATION piProcessInfo;
+	memset(&siStartupInfo, 0, sizeof(siStartupInfo));
+	memset(&piProcessInfo, 0, sizeof(piProcessInfo));
+	siStartupInfo.cb = sizeof(siStartupInfo);
+
+	if (CreateProcessW(
+		FullPathToExe.c_str(),
+		pwszParam, 
+		0, 
+		0, 
+		false,
+		CREATE_DEFAULT_ERROR_MODE, 
+		0, 
+		wPath.c_str(),
+		&siStartupInfo, 
+		&piProcessInfo) != false)
+	{
+		/* Watch the process. */
+		dwExitCode = WaitForSingleObject(piProcessInfo.hProcess, (SecondsToWait * 1000));
+	}
+	else
+	{
+		/* CreateProcess failed */
+		iReturnVal = GetLastError();
+	}
+
+	/* Free memory */
+	delete[]pwszParam;
+	pwszParam = 0;
+
+	/* Release handles */
+	CloseHandle(piProcessInfo.hProcess);
+	CloseHandle(piProcessInfo.hThread);
+
+	return iReturnVal;
 }
-void callXST() {
-	printf("\n\nExecuting Synthesis\n");
-	std::string outputFolder = "C:/Users/Nick/Documents/XilinxProjects/SingleItemTest/xst";
-	CreateDirectoryWithUserFullControlACL(outputFolder);
-	outputFolder = "C:/Users/Nick/Documents/XilinxProjects/SingleItemTest/xst/projnav.tmp";
-	CreateDirectoryWithUserFullControlACL(outputFolder);
-	//CreateDirectoryA((outputFolder + "xst/").c_str(), &sa);
-	//CreateDirectoryA((outputFolder + "xst/projnav.tmp").c_str(), &sa);
-	system("xst -ifn C:/Users/Nick/Documents/XilinxProjects/SingleItemTest/item.xst -ofn C:/Users/Nick/Documents/XilinxProjects/SingleItemTest/item.syr");
+void makeDirectories() {
+	std::string outputFolder = path + "xst";
+	CreateDirectoryA(outputFolder.c_str(), NULL);
+	outputFolder = path + "xst/projnav.tmp";
+	CreateDirectoryA(outputFolder.c_str(), NULL);
 }
 void makeInitFiles() {
 	std::ofstream prjFile;
-	prjFile.open("C:/Users/Nick/Documents/XilinxProjects/SingleItemTest/item.prj");
+	prjFile.open(path + "item.prj");
 	prjFile << "vhdl work \"item.vhf\"";
 	prjFile.close();
 
 	std::ofstream syrFile;
-	syrFile.open("C:/Users/Nick/Documents/XilinxProjects/SingleItemTest/item.syr");
+	syrFile.open(path + "item.syr");
 	syrFile.close();
 
 	std::ofstream xstFile;
-	xstFile.open("C:/Users/Nick/Documents/XilinxProjects/SingleItemTest/item.xst");
+	xstFile.open(path + "item.xst");
 	xstFile << "set -tmpdir \"xst/projnav.tmp\"\n";
 	xstFile << "set -xsthdpdir \"xst\"\n";
 	xstFile << "run\n";
-	xstFile << "-ifn item.prj\n";
+	xstFile << ("-ifn " + path + "item.prj\n").c_str();
 	xstFile << "-ifmt mixed\n";
 	xstFile << "-ofn item\n";
 	xstFile << "-ofmt NGC\n";
@@ -139,20 +176,26 @@ void makeInitFiles() {
 	xstFile << "-slice_utilization_ratio_maxmargin 5\n";
 	xstFile.close();
 }
-void deleteFiles() {
-	std::remove("C:/Users/Nick/Documents/XilinxProjects/SingleItemTest/item.prj");
-	std::remove("C:/Users/Nick/Documents/XilinxProjects/SingleItemTest/item.syr");
-	std::remove("C:/Users/Nick/Documents/XilinxProjects/SingleItemTest/item.xst");
+void deleteResources() {
+	std::remove((path + "item.prj").c_str());
+	std::remove((path + "item.syr").c_str());
+	std::remove((path + "item.xst").c_str());
+	std::remove((path + "item.vhf").c_str());
 }
 int main()
 {
 	printf("Checking if processor is available...");
 	if (system(NULL)) puts("Ok");
 	else exit(EXIT_FAILURE);
-	deleteFiles();
+	deleteResources();
+	makeDirectories();
 	makeInitFiles();
 	convertSchematicToHDL();
-	callXST();
+	//callXST();
+	std::wstring xstPath = L"C:\\Xilinx\\14.7\\ISE_DS\\ISE\\bin\\nt\\xst.exe";
+	std::wstring args = L"-ifn C:/Users/Nick/Desktop/NickTop/HomeWork/MASc/FPGAAutomationImplementation/BitStreamParser/BitStreamParser/Xilinx/SingleItemTest/item.xst -ofn C:/Users/Nick/Desktop/NickTop/HomeWork/MASc/FPGAAutomationImplementation/BitStreamParser/BitStreamParser/Xilinx/SingleItemTest/item.syr";
+	//testXST(xstPath, args);
+	ExecuteProcess(xstPath, args, 10);
 	return 0;
 }
 
